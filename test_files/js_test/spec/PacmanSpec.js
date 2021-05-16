4f818a92f9aaa2bfcaf91859105db2968f704543
Yin Song <songniy@gmail.com>
2021-05-16 19:24:56 -0400

    passed all test

--- test_files/js_test/spec/PacmanSpec.js
+++ test_files/js_test/spec/PacmanSpec.js
@@ -1,28 +1,37 @@
-var main = require(process.argv[3]);
-
-describe( "pacman", function () {
-    var arrayEquality = function(first, second){
-        count = 0;
-        for (i = 0; i < second.length; i++) {
-            if(parseInt(second[i]) == parseInt(first[i])){
-                count = count + 1;
-            }
-        }
-        return (count == 3);
+console.log(process.argv[3]);
+//var main = require(process.argv[3]);
+var main = require('../../../starter_code/pacman');
+describe('pacman', function () {
+  var arrayEquality = function (first, second) {
+    count = 0;
+    for (i = 0; i < second.length; i++) {
+      if (parseInt(second[i]) == parseInt(first[i])) {
+        count = count + 1;
+      }
     }
-    beforeEach(function () {
-        jasmine.addCustomEqualityTester(arrayEquality);
-    });
+    return count == 3;
+  };
+  beforeEach(function () {
+    jasmine.addCustomEqualityTester(arrayEquality);
+  });
 
-    it("traverses generic.txt", function () {
-        expect(main.pacman("generic.txt")).toEqual([6,1,27]);
-    });
+  it('traverses generic.txt', function () {
+    expect(main.pacman('generic.txt')).toEqual([6, 1, 27]);
+  });
 
-    it("doesn't run for edge.txt", function () {
-        expect(main.pacman("edge.txt")).toEqual([-1,-1,0]);
-    });
+  it("doesn't run for edge.txt", function () {
+    expect(main.pacman('edge.txt')).toEqual([-1, -1, 0]);
+  });
 
-    it("traverses runtime.txt", function () {
-        expect(main.pacman("runtime.txt")).toEqual([2142,147,148]);
-    });
+  it('traverses simple.txt', function () {
+    expect(main.pacman('simple.txt')).toEqual([2, 0, 3]);
+  });
+
+  it('does move for nomove.txt', function () {
+    expect(main.pacman('nomove.txt')).toEqual([2, 2, 0]);
+  });
+
+  it('traverses runtime.txt', function () {
+    expect(main.pacman('runtime.txt')).toEqual([2142, 147, 148]);
+  });
 });
